eb89bf30ff8b828abedebff2bc5ac54f77e51332
Dominique Lopes <tech@dlopes.de>
2016-09-08 10:21:57 +0200

    Relicensing
    Apache Software License 2.0 -> Mozilla Public License 2.0

--- stock-facilitator/src/test/java/de/dlopes/stocks/facilitator/services/HTMLFileExtractorTest.java
+++ stock-facilitator/src/test/java/de/dlopes/stocks/facilitator/services/HTMLFileExtractorTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Dominique Lopes.
+ * All rights reserved. 
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public 
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Contributors:
+ *     Dominique Lopes - initial API and implementation
+ *******************************************************************************/
 package de.dlopes.stocks.facilitator.services;
 
 import static org.junit.Assert.assertEquals;
